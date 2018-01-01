016ea84c4f8587bd8ec52808e79464df256437fc
beny <jlbeny100@gmail.com>
2018-01-01 22:13:18 +0100

    annotate dto with jaxb annotations and ws endpoint (with jax-ws) que es una interface

--- src/main/java/com/trainings/ws/soap/dto/CreditCardInfo.java
+++ src/main/java/com/trainings/ws/soap/dto/CreditCardInfo.java
@@ -2,14 +2,26 @@ package com.trainings.ws.soap.dto;
 
 import java.util.Date;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
 
+@XmlType(name="CreditCardInfo")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class CreditCardInfo {
 
+	@XmlElement(name="cardNumber", required=true)
 	String cardNumber;
+	@XmlElement(name="expirtyDate", required=true)
 	private Date expirtyDate;
+	@XmlElement(name="firstName", required=true)
 	String firstName;
+	@XmlElement(name="lastName", required=true)
 	String lastName;
+	@XmlElement(name="secCode", required=true)
 	String secCode;
+	@XmlElement(name="Address", required=true)
 	String Address;
 
 	public String getCardNumber() {
