0e58e305d467f8efb9b1491948081cdc4c6fc85f
beny <jlbeny100@gmail.com>
2018-01-11 00:12:16 +0100

    generate SoapFault from Runtime Exception
    - este SoapFault no aparece en el wsdl

--- src/main/java/com/trainings/ws/soap/PaymentProcessorImpl.java
+++ src/main/java/com/trainings/ws/soap/PaymentProcessorImpl.java
@@ -1,5 +1,7 @@
 package com.trainings.ws.soap;
 
+import org.apache.cxf.common.util.StringUtils;
+
 import com.trainings.ws.soap.dto.PaymentProcessorRequest;
 import com.trainings.ws.soap.dto.PaymentProcessorResponse;
 
@@ -9,6 +11,11 @@ public class PaymentProcessorImpl implements PaymentProcessor {
 	public PaymentProcessorResponse processPayment(
 			PaymentProcessorRequest paymentProcessorRequest) {
 		PaymentProcessorResponse paymentProcessorResponse = new PaymentProcessorResponse();
+		
+		String cardNumber = paymentProcessorRequest.getCreditCardInfo().getCardNumber();
+		if(StringUtils.isEmpty(cardNumber)) {
+			throw new RuntimeException("Invalid Card Number");
+		}
 		//Business Logic or a call  to a Business Logic Class Goes Here.
 		paymentProcessorResponse.setResult(true);
 		return paymentProcessorResponse;
