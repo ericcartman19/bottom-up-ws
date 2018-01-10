7774716ca93374203b8c85397d92cdca78cfde55
beny <jlbeny100@gmail.com>
2018-01-11 00:26:47 +0100

    declare customized exception
    - esta tambien aparece en el wsdl y en el soapFault que
    recibe el cliente

--- src/main/java/com/trainings/ws/soap/PaymentProcessor.java
+++ src/main/java/com/trainings/ws/soap/PaymentProcessor.java
@@ -6,11 +6,12 @@ import javax.jws.WebService;
 
 import com.trainings.ws.soap.dto.PaymentProcessorRequest;
 import com.trainings.ws.soap.dto.PaymentProcessorResponse;
+import com.trainings.ws.soap.exception.ServiceException;
 
 @WebService(name = "PaymentProcessor")
 public interface PaymentProcessor {
 
 	// @WebParam is not mandatory
 	public @WebResult(name="response") PaymentProcessorResponse processPayment(
-			@WebParam(name = "paymentProcessorRequest") PaymentProcessorRequest paymentProcessorRequest) throws Exception;
+			@WebParam(name = "paymentProcessorRequest") PaymentProcessorRequest paymentProcessorRequest) throws ServiceException;
 }
--- src/main/java/com/trainings/ws/soap/PaymentProcessorImpl.java
+++ src/main/java/com/trainings/ws/soap/PaymentProcessorImpl.java
@@ -4,17 +4,19 @@ import org.apache.cxf.common.util.StringUtils;
 
 import com.trainings.ws.soap.dto.PaymentProcessorRequest;
 import com.trainings.ws.soap.dto.PaymentProcessorResponse;
+import com.trainings.ws.soap.exception.ServiceException;
 
 
 public class PaymentProcessorImpl implements PaymentProcessor {
 
 	public PaymentProcessorResponse processPayment(
-			PaymentProcessorRequest paymentProcessorRequest) {
+			PaymentProcessorRequest paymentProcessorRequest) throws ServiceException{
+		
 		PaymentProcessorResponse paymentProcessorResponse = new PaymentProcessorResponse();
 		
 		String cardNumber = paymentProcessorRequest.getCreditCardInfo().getCardNumber();
 		if(StringUtils.isEmpty(cardNumber)) {
-			throw new RuntimeException("Invalid Card Number");
+			throw new ServiceException("Invalid Card Number");
 		}
 		//Business Logic or a call  to a Business Logic Class Goes Here.
 		paymentProcessorResponse.setResult(true);
